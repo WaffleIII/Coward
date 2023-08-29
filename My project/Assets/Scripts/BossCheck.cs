21d940f1df97ae252a71e25b1becf39143c58b42
WaffleIII <shacharherscu7@gmail.com>
2023-08-29 10:57:51 -0400

    Boss Teleporter "Fix" V1

--- My project/Assets/Scripts/BossCheck.cs
+++ My project/Assets/Scripts/BossCheck.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BossCheck : MonoBehaviour
 {
+    public GameObject tp;
+
     private void Update()
     {
         GameObject[] EnemysLeft = GameObject.FindGameObjectsWithTag("Enemy");
@@ -11,6 +13,8 @@ public class BossCheck : MonoBehaviour
         if (EnemysLeft.Length <= 0)
         {
             Debug.Log("YOU KILLED THEM ALL >:(");
+            Instantiate(tp, new Vector3(-6.81f, -3.62f, 0f), Quaternion.identity);
+            
         }
     }
 }
