f2b37bb0aafa30f547367d1c377599cd132825af
Squiddy24 <130530087+Squiddy24@users.noreply.github.com>
2023-08-29 11:08:43 -0400

    Fixed it

--- My project/Assets/Scripts/BossCheck.cs
+++ My project/Assets/Scripts/BossCheck.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class BossCheck : MonoBehaviour
 {
     public GameObject tp;
+    private bool hasSpawned = false;
 
     private void Update()
     {
         GameObject[] EnemysLeft = GameObject.FindGameObjectsWithTag("Enemy");
 
-        if (EnemysLeft.Length <= 0)
+        if (EnemysLeft.Length <= 0 && !hasSpawned)
         {
             Debug.Log("YOU KILLED THEM ALL >:(");
             Instantiate(tp, new Vector3(-6.81f, -3.62f, 0f), Quaternion.identity);
+            hasSpawned = true;
             
         }
     }
